498c164b8f85b57061fef6b9fca752a8d5170e9e
Mikael Bertze <mikael@bertze.com>
2015-12-07 21:00:49 +0100

    cleaning code

--- tests/ElectricityTickSensor_tests.py
+++ tests/ElectricityTickSensor_tests.py
@@ -1,11 +1,14 @@
 from nose.tools import *
 from homesensors.electricitysensors import ElectricityTickSensor
 
+
 def setup():
     print "ElectricityTickSensor - Tests"
 
+
 def teardown():
     pass
+
 
 def test_ticksPerKwh():
     sensor = ElectricityTickSensor(12, 1000)
