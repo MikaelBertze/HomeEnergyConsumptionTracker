d1a80ba42860c3b062cceab0ce3fedd768b4c3c3
Mikael Bertze <mikael@bertze.com>
2015-12-07 20:23:20 +0100

    Cleaning up code. No logic changes

--- sensor_simulators/realTimeElectricityTickSensorSimulator.py
+++ sensor_simulators/realTimeElectricityTickSensorSimulator.py
@@ -2,13 +2,14 @@ import sensorSimulatorUtils
 import thread
 from homesensors.electricitysensors import ElectricityTickSensor
 
-def StartSimulator(sensor_id, mean_value, span, period, sendSpan):
-    
-    sensor = ElectricityTickSensor(sensor_id, 1000)
-    sensorSimulatorUtils.RealTimeElectricityData(sensor, mean_value, span, period, sendSpan)
-    #thread.start_new_thread(sensorSimulatorUtils.RealTimeElectricityData, (sensor, mean_value, span, period, sendSpan))
 
-StartSimulator(1, 2, 2, 240, 10)
+def start_simulator(sensor_id, mean_value, span, period, sendSpan):
+    sensor = ElectricityTickSensor(sensor_id, 1000)
+    sensorSimulatorUtils.real_time_electricity_data(sensor, mean_value, span, period, sendSpan)
+    # thread.start_new_thread(sensorSimulatorUtils.RealTimeElectricityData, (sensor, mean_value, span, period, sendSpan))
+
+
+start_simulator(1, 2, 2, 240, 10)
 
 while 1:
-   pass
+    pass
--- sensor_simulators/sensorSimulatorUtils.py
+++ sensor_simulators/sensorSimulatorUtils.py
@@ -1,93 +1,67 @@
-import json
-from time import time, sleep
+from time import time
 import numpy as np
 import math
 import pause
-import homesensors.webserviceutils as webutils 
+import homesensors.webserviceutils as webutils
 
 webutils.url = "http://127.0.0.1:4000/v1/currentLoad/"
 webutils.DEBUG = True
 
-def buildAddDataMessage(data):
-	dataMsg = {"type" : "ElectricityTicks", "data" : data}
-	return dataMsg
 
-def sendTestData(sensor):
-	stop = False
-	while stop == False and sensor.canPopMessage():
-		message = sensor.peakDataMessage()
-		#print message
-		data = buildAddDataMessage(message)
-		stop = not webutils.sendMessage(message, sensor.sensorId())
-		if not stop:
-			sensor.popDataMessage()
-
-def getSinIndex(t, span, len):
-	tprim = t%span
-	index = float(tprim)/span * len
-	return int(index)
-
-def Make240minSquareWawe(channel, mean, span, period):
-	data = [mean - span/2, mean + span/2]
-	tStop = time()
-	t = tStop - 240*60
-	while t < tStop:
-		i = getSinIndex(t, period, len(data))
-		addEdgeTime(channel,t)
-		t = t + data[i]
-
-	sendTestData(1000)
-
-	return data
-
-def Make240minSinusData(channel, mean, span, period):
-	data = [math.sin(x) * (span/2) + mean for x in np.linspace(0,2*math.pi, num=2000)]
-	#print data
-	tStop = time()
-	t = tStop - 60*240
-	while t < tStop:
-		i = getSinIndex(t, period, len(data))
-		addEdgeTime(channel,t, 1000)
-		t = t + data[i]
-
-	sendTestData(1000)
+def build_add_data_message(data):
+    datamsg = {"type": "ElectricityTicks", "data": data}
+    return datamsg
 
 
-def RealTimeElectricityData(sensor, mean, span, period, sendSpan):
-	"""Real time electricity tick sensor simulator.
-	This simulator will mimic a real tick sensor.
-	Output from this sensor will be a sinus wave.
-	sensor - instance of an ElectricityTickSensor
-	mean - mean value for the sinus wave
-	span - peak to peak distance
-	period - time span in seconds for full sinus wave
-	sendSpan - time span in seconds between send to web service
-	"""
+def send_test_data(sensor):
+    stop = False
+    while stop is False and sensor.canPopMessage():
+        message = sensor.peakDataMessage()
+        data = build_add_data_message(message)
+        stop = not webutils.sendMessage(data, sensor.sensorId())
+        if not stop:
+            sensor.popDataMessage()
 
-	print "Starting simulator"
-	print "------------------------------"
-	print "Type: Electricity Tick Sensor"
-	print "Mean: %i sec"%mean
-	print "Span: %i Watt"%span
-	print "Period: %i sec"%period
-	print "Send Interval: %i"%sendSpan
-	print "-----------------------------"
-	print
 
-	data = [math.sin(x) * (span/2) + mean for x in np.linspace(0,2*math.pi, num=2000)]
-	t = time()
-	lastSend = t
-	while True:
-		i = getSinIndex(t, period, len(data))
-		delta = data[i]
-		t = t + delta
-		pause.until(t)
-		w = sensor.convertToWatt(delta)
-		print "Current load: " + str(w)
-		sensor.addValue(w)
+def get_sin_index(t, span, len):
+    tprim = t % span
+    index = float(tprim) / span * len
+    return int(index)
 
-		if time() - lastSend >= sendSpan:
-			sendTestData(sensor)
-			lastSend = time()
-			
 
+def real_time_electricity_data(sensor, mean, span, period, sendSpan):
+    """Real time electricity tick sensor simulator.
+    This simulator will mimic a real tick sensor.
+    Output from this sensor will be a sinus wave.
+    sensor - instance of an ElectricityTickSensor
+    mean - mean value for the sinus wave
+    span - peak to peak distance
+    period - time span in seconds for full sinus wave
+    sendSpan - time span in seconds between send to web service
+    """
+
+    print "Starting simulator"
+    print "------------------------------"
+    print "Type: Electricity Tick Sensor"
+    print "Mean: %i sec" % mean
+    print "Span: %i Watt" % span
+    print "Period: %i sec" % period
+    print "Send Interval: %i" % sendSpan
+    print "-----------------------------"
+    print
+
+    data = [math.sin(x) * (span / 2) + mean for x in np.linspace(0, 2 * math.pi, num=2000)]
+    t = time()
+    last_send= t
+    while True:
+        i = get_sin_index(t, period, len(data))
+        delta = data[i]
+        t += delta
+        pause.until(t)
+        w = sensor.convertToWatt(delta)
+        print "Current load: " + str(w)
+        sensor.addValue(w)
+
+        if time() - last_send >= sendSpan:
+            send_test_data(sensor)
+            last_send = time()
