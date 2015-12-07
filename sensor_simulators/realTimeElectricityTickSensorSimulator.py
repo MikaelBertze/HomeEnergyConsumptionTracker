4f16cebda663183a3c1bd73a73abea25e97f8121
Mikael Bertze <mikael@bertze.com>
2015-12-07 20:25:35 +0100

    Code cleanup

--- sensor_simulators/realTimeElectricityTickSensorSimulator.py
+++ sensor_simulators/realTimeElectricityTickSensorSimulator.py
@@ -1,13 +1,10 @@
 import sensorSimulatorUtils
-import thread
 from homesensors.electricitysensors import ElectricityTickSensor
 
 
-def start_simulator(sensor_id, mean_value, span, period, sendSpan):
+def start_simulator(sensor_id, mean_value, span, period, send_span):
     sensor = ElectricityTickSensor(sensor_id, 1000)
-    sensorSimulatorUtils.real_time_electricity_data(sensor, mean_value, span, period, sendSpan)
-    # thread.start_new_thread(sensorSimulatorUtils.RealTimeElectricityData, (sensor, mean_value, span, period, sendSpan))
-
+    sensorSimulatorUtils.real_time_electricity_data(sensor, mean_value, span, period, send_span)
 
 start_simulator(1, 2, 2, 240, 10)
 
