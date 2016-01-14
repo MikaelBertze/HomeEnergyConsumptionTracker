a21dea078bab0704e989a4b0a76be39e8fdcdfab
Mikael Bertze <mikael@bertze.com>
2016-01-14 22:46:28 +0100

    The electricity sensor simulator now outputs a sinus wave (power).

--- sensor_simulators/realTimeElectricityTickSensorSimulator.py
+++ sensor_simulators/realTimeElectricityTickSensorSimulator.py
@@ -4,9 +4,9 @@ from homesensors.electricitysensors import ElectricityTickSensor
 
 def start_simulator(sensor_id, mean_value, span, period, send_span):
     sensor = ElectricityTickSensor(sensor_id, 1000)
-    sensorSimulatorUtils.real_time_electricity_data(sensor, mean_value, span, period, send_span)
+    sensorSimulatorUtils.real_time_electricity_data_specified_by_power(sensor, mean_value, span, period, send_span)
 
-start_simulator(1, 2, 2, 240, 10)
+start_simulator(1, 1000, 500, 30, 10)
 
 while 1:
     pass
--- sensor_simulators/sensorSimulatorUtils.py
+++ sensor_simulators/sensorSimulatorUtils.py
@@ -28,39 +28,61 @@ def get_sin_index(t, span, len):
     return int(index)
 
 
-def real_time_electricity_data(sensor, mean, span, period, sendSpan):
+def real_time_electricity_data_specified_by_power(sensor, mean_power, power_span, period, send_span):
     """Real time electricity tick sensor simulator.
     This simulator will mimic a real tick sensor.
     Output from this sensor will be a sinus wave.
     sensor - instance of an ElectricityTickSensor
-    mean - mean value for the sinus wave
-    span - peak to peak distance
+    mean_power - mean power value (Watt)
+    power_span - peak to peak distance (Watt)
     period - time span in seconds for full sinus wave
-    sendSpan - time span in seconds between send to web service
+    send_span - time span in seconds between send to web service
+    """
+
+    w_per_tick = 3600.0 / sensor.ticks_per_kWh() *1000
+
+    curve = get_curve(mean_power, power_span)
+
+    real_time_electricity_simulator(sensor, curve, period, send_span)
+
+
+def get_curve(mean, span):
+    return [math.sin(x) * (span / 2) + mean for x in np.linspace(0, 2 * math.pi, num=2000)]
+
+
+def real_time_electricity_simulator(sensor, curve, period, send_span):
+    """Real time electricity tick sensor simulator.
+    This simulator will mimic a real tick sensor.
+    Output from this sensor will be a sinus wave.
+    sensor - instance of an ElectricityTickSensor
+    mean - mean value for the sinus wave (time between ticks in seconds)
+    span - peak to peak distance (seconds)
+    period - time span in seconds for full sinus wave
+    send_span - time span in seconds between send to web service
     """
 
     print "Starting simulator"
     print "------------------------------"
     print "Type: Electricity Tick Sensor"
-    print "Mean: %i sec" % mean
-    print "Span: %i Watt" % span
+    print "Max: %f sec" % max(curve)
+    print "Min: %f sec" % min(curve)
     print "Period: %i sec" % period
-    print "Send Interval: %i" % sendSpan
+    print "Send Interval: %i" % send_span
     print "-----------------------------"
     print
 
-    data = [math.sin(x) * (span / 2) + mean for x in np.linspace(0, 2 * math.pi, num=2000)]
     t = time()
     last_send= t
     while True:
-        i = get_sin_index(t, period, len(data))
-        delta = data[i]
-        t += delta
+        i = get_sin_index(t, period, len(curve))
+        power = curve[i]
+        diff = sensor.convert_to_time(power)
+        t += diff
         pause.until(t)
-        w = sensor.convert_to_watt(delta)
-        print "Current load: " + str(w)
-        sensor.add_value(w)
+        print "tick time: " + str(diff)
+        print "Current load: " + str(power)
+        sensor.add_value(power)
 
-        if time() - last_send >= sendSpan:
+        if time() - last_send >= send_span:
             send_test_data(sensor)
             last_send = time()
