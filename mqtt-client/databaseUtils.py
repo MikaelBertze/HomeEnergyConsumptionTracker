7b3bac5220642139fce57a32b6b87c4103129fb7
Mikael Bertze <mikael@bertze.com>
2020-01-25 23:46:24 +0100

    Mqtt client that store recieved data in database

---
+++ mqtt-client/databaseUtils.py
@@ -0,0 +1,66 @@
+
+import pymysql
+
+from scipy.interpolate import interp1d
+from scipy import arange, array, exp
+
+
+databaseUser = ""
+databasePass = ""
+databaseDb = ""
+
+
+def connect():
+    return pymysql.connect(user=databaseUser, passwd=databasePass,db=databaseDb)
+
+
+def insert_many(query, data):
+    try:
+        db = connect()
+        c=db.cursor()
+        c.executemany(query, data)
+        db.commit()
+        db.close()
+    except Exception as e:
+        print("SQL error: " + e.message)
+
+
+def query_for_data(query):
+    rows = []
+    try:
+        db = connect()
+        c=db.cursor()
+        c.execute(query)
+        rows = c.fetchall()
+        db.commit()
+        db.close()
+    except:
+        print("SQL error")
+    return rows
+
+
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def extrap1d(interpolator):
+    xs = interpolator.x
+    ys = interpolator.y
+
+    def pointwise(x):
+        if x < xs[0]:
+            return ys[0]+(x-xs[0])*(ys[1]-ys[0])/(xs[1]-xs[0])
+        elif x > xs[-1]:
+            return ys[-1]+(x-xs[-1])*(ys[-1]-ys[-2])/(xs[-1]-xs[-2])
+        else:
+            return interpolator(x)
+
+
+    def ufunclike(xs):
+        return array(map(pointwise, array(xs)))
+
+    return ufunclike
--- mqtt-client/mqtt-client.py
+++ mqtt-client/mqtt-client.py
@@ -1,15 +1,17 @@
+from datetime import datetime
 import paho.mqtt.client as mqtt
 from homesensors.electricitysensors import ElectricityTickSensor
-import homesensors.webserviceutils as webutils
-
+import deliveryService
+import configparser
+import databaseUtils
 
 def send_data(sensor):
-    stop = False
-    while stop is False and sensor.can_pop_message():
-        message = sensor.peak_data_message()
-        stop = not webutils.send_message(message, sensor.sensor_id())
-        if not stop:
-            sensor.pop_data_message()
+    while sensor.can_pop_message():
+        message = sensor.pop_data_message()
+
+        date_time= datetime.strptime(message['time'], '%Y-%m-%d %H:%M:%S.%f')
+    
+        deliveryService.current_load_delivery(sensor.sensor_id(), message['sensorType'], message['value'], date_time)
 
 
 
@@ -23,19 +25,33 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    s = msg.payload.decode('ascii')
-    key, value = (x.strip() for x in s.split(':'))
-    if key == 'tickPeriod':
-        v = int(value)
-        wh_per_hit = 1 / float(1000) * 1000
-        power = wh_per_hit * 3600/float(v/1000)
-        sensor.add_value(power)
-        send_data(sensor)
+    try:
+        print(msg.topic+" "+str(msg.payload))
+        s = msg.payload.decode('ascii')
+        key, value = (x.strip() for x in s.split(':'))
+        if key == 'tickPeriod':
+            v = int(value)
+            wh_per_hit = 1 / float(1000) * 1000
+            power = wh_per_hit * 3600/float(v/1000)
+            print(power)
+            sensor.add_value(power)
+            send_data(sensor)
+    except Exception as ex:
+        print(ex)
+        
 
 sensor = ElectricityTickSensor(1, 1000)
-webutils.url = "http://127.0.0.1:4000/v1/currentLoad/"
-webutils.DEBUG = True
+#webutils.url = "http://127.0.0.1:4000/v1/currentLoad/"
+#webutils.DEBUG = True
+
+config = configparser.RawConfigParser()
+config.read('mqtt-client.cfg')
+
+#port = config.getint("Site", "port")
+#url = config.get("Site", "url")
+databaseUtils.databaseUser = config.get("DB", "user")
+databaseUtils.databasePass = config.get("DB", "pass")
+databaseUtils.databaseDb = config.get("DB", "database")
 
 client = mqtt.Client()
 client.on_connect = on_connect
