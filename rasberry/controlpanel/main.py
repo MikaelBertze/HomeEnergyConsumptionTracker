105edc711292644a0ef533e5d169edf51ead46e9
Mikael Bertze <mikael@bertze.com>
2020-03-21 19:10:37 +0100

    using env variables for fullscreen mode

--- rasberry/controlpanel/main.py
+++ rasberry/controlpanel/main.py
@@ -1,39 +1,27 @@
 # -*- coding: utf-8 -*-
+import os
 from loguru import logger
-from rx import create
-
 from controlpanel.frames import MainApp
 from rx.subject import Subject
-
 from controlpanel.updaters import MqttUpdater, WeatherUpdater
 
-
-def get_config():
-    config = {}
-    with open('home_display.cfg', 'r') as config_file:
-        content = config_file.readlines()
-    for c in content:
-        row = [x.strip() for x in c.split(':')]
-        config[row[0]] = row[1]
-    return config
 
 if __name__ == '__main__':
     logger.add("home_display.log", retention="5 days")
     logger.info("Starting up!")
-    config = get_config()
-    logger.info("Configuration: " + str(config))
-
     updates = Subject()
     updates.subscribe(lambda x: print(x))
 
     mqtt_updater = MqttUpdater(updates)
     mqtt_updater.start()
-
     weather_updater = WeatherUpdater(updates)
     weather_updater.start()
 
     app = MainApp(updates)
-    if config['fullscreen'] == "YES":
+
+    if os.getenv('CONTROLPANEL_FULLSCREEN') == '1':
+        logger.info("Setting full screen mode")
         app.attributes("-fullscreen", True)
+
     app.geometry("720x480")
     app.mainloop()
